23412e4a9bb3bd75b36c86bd4e7f83de835465e1
EnzoVezzaro <enzo-vezzaro@live.it>
2018-04-02 12:46:03 +0200

    fix other projects

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -45,6 +45,18 @@ module.exports = {
       description: 'Corporate Website',
       body: 'The CannAlliance USA is an advocacy and trade association of cannabis farmers, producers, manufacturers, patients, and responsible cannabis businesses that recognize the economic and medicinal value that cannabis culture has brought to California County.',
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -53,8 +65,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'cannalliance.jpg',
       keywords: {
@@ -69,6 +85,18 @@ module.exports = {
       description: 'Web Consulting Agency',
       body: 'INSULA | Digital Studio is a online Web Consulting Agency focus on creating web products and services for the modern web.',
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -77,8 +105,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'insula-digital-studio.jpg',
       keywords: {
@@ -93,6 +125,18 @@ module.exports = {
       description: 'Movie',
       body: 'LA RAZANTE is a film that narrates a common drama not only in societies like those of the Dominican Republic but anywhere in the world, it is the story of the abandonment suffered by many children, parents who are absent leaving the whole weight of parenting to mothers and that you are always hoping that these abandoned children will idealize the absent father.',
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -101,8 +145,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'larazante.jpg',
       keywords: {
@@ -117,6 +165,18 @@ module.exports = {
       description: 'Online Pool',
       body: 'Political Website for online questionary about the Presidential Election in the Dominican Repiblic in 2016.',
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -125,8 +185,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'presidente-2018.jpg',
       keywords: {
@@ -141,6 +205,18 @@ module.exports = {
       description: 'Hobby',
       body: "I was born in the 90's. When 'The Fresh Prince of Bel Air' was the coolest TV show ever. When kids used to go outside to play ball. When the only social network available were Latin Chat, Hi5 or MySpace...and ( of course! ) the old, realible, immortal CONSOLES. This is a website I made for Fun to show the 4 consoles that were part of my childhood...and I'm sure yours too!",
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -149,8 +225,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'consoles.jpg',
       keywords: {
@@ -165,6 +245,18 @@ module.exports = {
       description: 'Movie',
       body: 'Dominican Film, now in premier in the United States, about the life and struggles of 3 friends trying to get ahead with their musical projects fighting for a better life and better future.',
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -173,8 +265,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'voces-de-la-calle.jpg',
       keywords: {
@@ -189,6 +285,18 @@ module.exports = {
       description: 'Vape Store',
       body: 'E-commerce Vape Shop with big present in the Latin American Market. With more than 100 products online and hundreds of customers, Vape Cloud Shop built its audience overtime offering them great customer service and support.',
       client: 'Greg MacDonal',
+      section_1:{
+        title: 'Detail Retina screen, Loupe resource.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_2:{
+        title: 'Introducing your UX platform.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      section_3:{
+        title: 'Smart Phone and Tablet Responsive screens.',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
       services:[
         'HTML5',
         'CSS',
@@ -197,8 +305,12 @@ module.exports = {
         'GITHUB PAGES',
       ],
       images: {
-        introWindow: '../assets/img/portfolio/test-intro.png',
-        introPhone: '../assets/img/portfolio/test-intro-phone.png',
+        introWindow: 'art-project-united-intro-window.png',
+        introPhone: 'art-project-united-intro-phone.png',
+        section1Window: 'art-project-united-section-1-window.png',
+        section2Window: 'test-phones-section-3.png',
+        section3Tablet: 'art-project-united-section-4-tablet.png',
+        section3Phone: 'art-project-united-section-3-phone.png',
       },
       thumbnail: 'vape-cloud-shop.jpg',
       keywords: {

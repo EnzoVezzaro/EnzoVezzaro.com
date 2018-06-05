cca9e1fabf749967eb93a8ca925cc490148f307c
EnzoVezzaro <enzo-vezzaro@live.it>
2018-06-05 23:10:19 +0200

    updated resume

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ import {
   Link
 } from 'react-router-dom';
 import './Menu.css';
-import PDF from '../assets/Resume-ilovepdf-compressed.pdf';
+import PDF from '../assets/Resume-compressed.pdf';
 
 class Menu extends Component {
   constructor(props){
